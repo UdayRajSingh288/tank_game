4dd83b5eddca6a461237220c3bc203a6605ce845
udayrajsingh288 <udayrajsingh288@gmail.com>
2025-06-17 10:09:57 +0530

    main menu completed

--- src/game_main.c
+++ src/game_main.c
@@ -8,11 +8,11 @@ static const char *app_name = "Tank Game";
 static const char *app_ver = "1.0";
 static const char *logname = "game_log.txt";
 static const char *game_title_text = "TANK GAME";
-static const char *singleplayer_button_text = "Singleplayer Mode";
-static const char *multiplayer_host_button_text = "Host multiplayer game server";
-static const char *multiplayer_client_button_text = "Join multiplayer game server";
-static const char *creator_button_text = "Meet the creator";
-static const char *quit_button_text = "Quit game";
+static const char *singleplayer_button_text = "Singleplayer";
+static const char *multiplayer_host_button_text = "Host server";
+static const char *multiplayer_client_button_text = "Join server";
+static const char *creator_button_text = "Creator";
+static const char *quit_button_text = "Quit";
 const char *sand_img = "sprites/sand.bmp";
 const char *char_img = "sprites/char.bmp";
 
@@ -20,9 +20,11 @@ static inline int main_menu(SDL_Window *window, SDL_Renderer *renderer, FILE *lo
 	SDL_Surface *surface;
 	SDL_Texture *sand_tex, *char_tex;
 	SDL_Event event;
-	SDL_FRect sp_rect, mphost_rect, mpclient_rect, cr_rect, qt_rect;
+	SDL_FRect t_rect, sp_rect, mphost_rect, mpclient_rect, cr_rect, qt_rect;
 	SDL_FPoint mp;
-	int w, h, bw, bh, x, mstate;
+	union rgb u;
+	int w, h, bw, bh, x, r;
+	bool sp_focus, mphost_focus, mpclient_focus, cr_focus, qt_focus;
 
 	surface = SDL_LoadBMP(sand_img);
 	if (NULL == surface){
@@ -54,21 +56,16 @@ static inline int main_menu(SDL_Window *window, SDL_Renderer *renderer, FILE *lo
 		fprintf(logfp, "SDL_GetWindowSize() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
 		return ERROR_MM;
 	}
-	if (EXIT_FAILURE == draw_tiled_background(logfp, renderer, sand_tex, w, h, SAND_IMG_W, SAND_IMG_H)){
-		fprintf(logfp, "draw_bg() error file game_main.c line %d\n", __LINE__);
-		return ERROR_MM;
-	}
-	SDL_DestroyTexture(sand_tex);
 	bh = h / 14;
 	bw = w / 2;
 	x = ((w - bw) / 2);
+	t_rect.w = (float)bw;
+	t_rect.h = (float)(2 * bh);
+	t_rect.x = (float)x;
+	t_rect.y = (float)bh;
 	sp_rect.w = (float)bw;
-	sp_rect.h = (float)(2 * bh);
+	sp_rect.h = (float)bh;
 	sp_rect.x = (float)x;
-	sp_rect.y = (float)bh;
-	if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, game_title_text, &sp_rect, TITLE_COLOR, TEXT_CENTER)){
-		fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-	}
 	sp_rect.y = (float)(4 * bh);
 	mphost_rect.w = (float)bw;
 	mphost_rect.h = (float)bh;
@@ -86,25 +83,170 @@ static inline int main_menu(SDL_Window *window, SDL_Renderer *renderer, FILE *lo
 	qt_rect.h = (float)bh;
 	qt_rect.x = (float)x;
 	qt_rect.y = (float)(12 * bh);
-	if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, singleplayer_button_text, &sp_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
-		fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-	}
-	if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, multiplayer_host_button_text, &mphost_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
-		fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-	}
-	if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, multiplayer_client_button_text, &mpclient_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
-		fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-	}
-	if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, creator_button_text, &cr_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
-		fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-	}
-	if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, quit_button_text, &qt_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
-		fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-	}
-	if (false == SDL_RenderPresent(renderer)){
-		fprintf(logfp, "SDL_RenderPresent() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
-	}
+	sp_focus = false;
+	mphost_focus = false;
+	mpclient_focus = false;
+	cr_focus = false;
+	qt_focus = false;
 	while (true){
+		if (EXIT_FAILURE == draw_tiled_background(logfp, renderer, sand_tex, w, h, SAND_IMG_W, SAND_IMG_H)){
+			fprintf(logfp, "draw_bg() error file game_main.c line %d\n", __LINE__);
+			return ERROR_MM;
+		}
+		if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, game_title_text, &t_rect, TITLE_COLOR, TEXT_CENTER)){
+			fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
+			return ERROR_MM;
+		}
+		if (sp_focus){
+			u.hex = BUTTON_UNFOCUSED_COLOR;
+			if (false == SDL_SetRenderDrawColor(renderer, u.r, u.g, u.b, 0xff)){
+				fprintf(logfp, "SDL_SetRenderDrawColor() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (false == SDL_RenderFillRect(renderer, &sp_rect)){
+				fprintf(logfp, "SDL_RenderFillRect() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, singleplayer_button_text, &sp_rect, BUTTON_FOCUSED_COLOR, TEXT_CENTER)){
+				fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
+				return ERROR_MM;
+			}
+		}
+		else {
+			u.hex = BUTTON_FOCUSED_COLOR;
+			if (false == SDL_SetRenderDrawColor(renderer, u.r, u.g, u.b, 0xff)){
+				fprintf(logfp, "SDL_SetRenderDrawColor() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (false == SDL_RenderFillRect(renderer, &sp_rect)){
+				fprintf(logfp, "SDL_RenderFillRect() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, singleplayer_button_text, &sp_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
+				fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
+				return ERROR_MM;
+			}
+		}
+		if (mphost_focus){
+			u.hex = BUTTON_UNFOCUSED_COLOR;
+			if (false == SDL_SetRenderDrawColor(renderer, u.r, u.g, u.b, 0xff)){
+				fprintf(logfp, "SDL_SetRenderDrawColor() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (false == SDL_RenderFillRect(renderer, &mphost_rect)){
+				fprintf(logfp, "SDL_RenderFillRect() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, multiplayer_host_button_text, &mphost_rect, BUTTON_FOCUSED_COLOR, TEXT_CENTER)){
+				fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
+				return ERROR_MM;
+			}
+		}
+		else {
+			u.hex = BUTTON_FOCUSED_COLOR;
+			if (false == SDL_SetRenderDrawColor(renderer, u.r, u.g, u.b, 0xff)){
+				fprintf(logfp, "SDL_SetRenderDrawColor() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (false == SDL_RenderFillRect(renderer, &mphost_rect)){
+				fprintf(logfp, "SDL_RenderFillRect() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, multiplayer_host_button_text, &mphost_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
+				fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
+				return ERROR_MM;
+			}
+		}
+		if (mpclient_focus){
+			u.hex = BUTTON_UNFOCUSED_COLOR;
+			if (false == SDL_SetRenderDrawColor(renderer, u.r, u.g, u.b, 0xff)){
+				fprintf(logfp, "SDL_SetRenderDrawColor() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (false == SDL_RenderFillRect(renderer, &mpclient_rect)){
+				fprintf(logfp, "SDL_RenderFillRect() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, multiplayer_client_button_text, &mpclient_rect, BUTTON_FOCUSED_COLOR, TEXT_CENTER)){
+				fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
+				return ERROR_MM;
+			}
+		}
+		else {
+			u.hex = BUTTON_FOCUSED_COLOR;
+			if (false == SDL_SetRenderDrawColor(renderer, u.r, u.g, u.b, 0xff)){
+				fprintf(logfp, "SDL_SetRenderDrawColor() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (false == SDL_RenderFillRect(renderer, &mpclient_rect)){
+				fprintf(logfp, "SDL_RenderFillRect() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, multiplayer_client_button_text, &mpclient_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
+				fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
+				return ERROR_MM;
+			}
+		}
+		if (cr_focus){
+			u.hex = BUTTON_UNFOCUSED_COLOR;
+			if (false == SDL_SetRenderDrawColor(renderer, u.r, u.g, u.b, 0xff)){
+				fprintf(logfp, "SDL_SetRenderDrawColor() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (false == SDL_RenderFillRect(renderer, &cr_rect)){
+				fprintf(logfp, "SDL_RenderFillRect() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, creator_button_text, &cr_rect, BUTTON_FOCUSED_COLOR, TEXT_CENTER)){
+				fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
+				return ERROR_MM;
+			}
+		}
+		else {
+			u.hex = BUTTON_FOCUSED_COLOR;
+			if (false == SDL_SetRenderDrawColor(renderer, u.r, u.g, u.b, 0xff)){
+				fprintf(logfp, "SDL_SetRenderDrawColor() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (false == SDL_RenderFillRect(renderer, &cr_rect)){
+				fprintf(logfp, "SDL_RenderFillRect() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, creator_button_text, &cr_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
+				fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
+				return ERROR_MM;
+			}
+		}
+		if (qt_focus){
+			u.hex = BUTTON_UNFOCUSED_COLOR;
+			if (false == SDL_SetRenderDrawColor(renderer, u.r, u.g, u.b, 0xff)){
+				fprintf(logfp, "SDL_SetRenderDrawColor() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (false == SDL_RenderFillRect(renderer, &qt_rect)){
+				fprintf(logfp, "SDL_RenderFillRect() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, quit_button_text, &qt_rect, BUTTON_FOCUSED_COLOR, TEXT_CENTER)){
+				fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
+				return ERROR_MM;
+			}
+		}
+		else {
+			u.hex = BUTTON_FOCUSED_COLOR;
+			if (false == SDL_SetRenderDrawColor(renderer, u.r, u.g, u.b, 0xff)){
+				fprintf(logfp, "SDL_SetRenderDrawColor() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (false == SDL_RenderFillRect(renderer, &qt_rect)){
+				fprintf(logfp, "SDL_RenderFillRect() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+				return ERROR_MM;
+			}
+			if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, quit_button_text, &qt_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
+				fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
+				return ERROR_MM;
+			}
+		}
 		if (false == SDL_WaitEvent(&event)){
 			fprintf(logfp, "SDL_WaitEvent() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
 			return ERROR_MM;
@@ -113,58 +255,66 @@ static inline int main_menu(SDL_Window *window, SDL_Renderer *renderer, FILE *lo
 			mp.x = event.motion.x;
 			mp.y = event.motion.y;
 			if (SDL_PointInRectFloat(&mp, &sp_rect)){
-				if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, singleplayer_button_text, &sp_rect, TITLE_COLOR, TEXT_CENTER)){
-					fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-				}
+				sp_focus = true;
 			}
 			else {
-				if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, singleplayer_button_text, &sp_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
-					fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-				}
+				sp_focus = false;
 			}
 			if (SDL_PointInRectFloat(&mp, &mphost_rect)){
-				if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, multiplayer_host_button_text, &mphost_rect, TITLE_COLOR, TEXT_CENTER)){
-					fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-				}
+				mphost_focus = true;
 			}
 			else {
-				if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, multiplayer_host_button_text, &mphost_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
-					fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-				}
+				mphost_focus = false;
 			}
 			if (SDL_PointInRectFloat(&mp, &mpclient_rect)){
-				if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, multiplayer_client_button_text, &mpclient_rect, TITLE_COLOR, TEXT_CENTER)){
-					fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-				}
+				mpclient_focus = true;
 			}
 			else {
-				if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, multiplayer_client_button_text, &mpclient_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
-					fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-				}
+				mpclient_focus = false;
 			}
 			if (SDL_PointInRectFloat(&mp, &cr_rect)){
-				if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, creator_button_text, &cr_rect, TITLE_COLOR, TEXT_CENTER)){
-					fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-				}
+				cr_focus = true;
 			}
 			else {
-				if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, creator_button_text, &cr_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
-					fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-				}
+				cr_focus = false;
 			}
 			if (SDL_PointInRectFloat(&mp, &qt_rect)){
-				if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, quit_button_text, &qt_rect, TITLE_COLOR, TEXT_CENTER)){
-					fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-				}
+				qt_focus = true;
 			}
 			else {
-				if (EXIT_FAILURE == draw_text(logfp, renderer, char_tex, quit_button_text, &qt_rect, BUTTON_UNFOCUSED_COLOR, TEXT_CENTER)){
-					fprintf(logfp, "draw_text() error file game_main.c line %d\n", __LINE__);
-				}
+				qt_focus = false;
 			}
 		}
+		if (SDL_EVENT_MOUSE_BUTTON_DOWN == event.type){
+			if (sp_focus){
+				r = SINGLEPLAYER_MM;
+				break;
+			}
+			if (mphost_focus){
+				r = MULTIPLAYER_HOST_MM;
+				break;
+			}
+			if (mpclient_focus){
+				r =  MULTIPLAYER_CLIENT_MM;
+				break;
+			}
+			if (cr_focus){
+				r =  CREATOR_MM;
+				break;
+			}
+			if (qt_focus){
+				r =  QUIT_MM;
+				break;
+			}
+		}
+		if (false == SDL_RenderPresent(renderer)){
+			fprintf(logfp, "SDL_RenderPresent() error file game_main.c line %d\n%s\n", __LINE__, SDL_GetError());
+			return ERROR_MM;
+		}
 	}
-	return QUIT_MM;
+	SDL_DestroyTexture(sand_tex);
+	SDL_DestroyTexture(char_tex);
+	return r;
 }
 
 
@@ -233,8 +383,15 @@ int main(int argc, char *argv[]){
 	if (game_init(&window, &renderer, &logfp)){
 		return EXIT_FAILURE;
 	}
-
 	rc = main_menu(window, renderer, logfp);
+	if (ERROR_MM == rc){
+		fprintf(logfp, "main_menu() error in file game_main.c line %d\n", __LINE__);
+		fclose(logfp);
+		SDL_DestroyRenderer(renderer);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return EXIT_FAILURE;
+	}
 	switch (rc){
 		case SINGLEPLAYER_MM:
 			break;
@@ -244,11 +401,9 @@ int main(int argc, char *argv[]){
 			break;
 		case CREATOR_MM:
 	}
-
 	fclose(logfp);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-
 	return EXIT_SUCCESS;
 }
